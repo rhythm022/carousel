bef4716909fbf2a8533a365a7c9f5188cd159ea4
rhythm022 <rhythm22@yeah.net>
2022-08-13 15:19:18 +0800

    save

--- main.js
+++ main.js
@@ -11,40 +11,46 @@ class Carousel extends Component {
             < img src="https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg" />
             < img src="https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg" />
             < img src="https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg" />
-
         </div>
 
-        let current = 0
 
-        setInterval(() => {
-            let next = (current + 1) % element.children.length
-            const currentEle = element.children[current]
-            const nextEle = element.children[next];
+        element.addEventListener('dragstart',(e)=>{
+            e.preventDefault()
+        })
 
-          
-            [...element.children].forEach(ele=>{
-                ele.style.zIndex = 0
-
+        let startX =0
+        let endX=0
+        const mousemove = (e) => {
+            console.log(e)
+            const offset = endX + e.clientX - startX;
+            [...element.children].forEach(ele => {
+                ele.style.transform = `translateX(${offset}px)`
+                ele.style.transition = 'none'
             })
-            currentEle.style.zIndex = 2
-            nextEle.style.zIndex = 2
 
-            currentEle.style.left = current * -100 + '%'
-            nextEle.style.left = (next - 1) * -100 + '%'
-            currentEle.style.transition = 'none'
-            nextEle.style.transition = 'none'
-   
+        }
+        const mouseup = (e) => {
+            console.log(e)
+            endX = endX + e.clientX - startX
 
-            setTimeout(() => {
-                currentEle.style.left = (current + 1) * -100 + '%'
-                nextEle.style.left = next * -100 + '%'
-                currentEle.style.transition = ''
-                nextEle.style.transition = ''
-                
-                current = next
-            },0)
-        }, 2000);
+            const index = Math.round(endX / 1142)
+            const offset = index * 1142;
 
+            [...element.children].forEach(ele => {
+                ele.style.transform = `translateX(${offset}px)`;
+                ele.style.transition = ''
+            })
+            endX = offset
+            document.removeEventListener('mousemove', mousemove)
+            document.removeEventListener('mouseup', mouseup)
+        }
+        element.addEventListener('mousedown', (e) => {
+            console.log(e)
+            startX = e.clientX
+            document.addEventListener('mousemove', mousemove)
+            document.addEventListener('mouseup', mouseup)
+        })
+        console.log(element)
         return element
     }
 }
